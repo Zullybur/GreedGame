c84e542c408a44f9a36c3f59faf6b143292139df
Matthew Casiro <mattcasiro@gmail.com>
2016-06-29 15:51:30 -0700

    Create code for calculating score of a single turn
    See GREED_RULES.txt for details on how the score is calculated.
    
    Refactored Score based on code review feeback
    
    - Moved operations out of main method to private helper methods
    
    - Re-wrote hash creation to be more verbose using .each instead of
      .inject
    
    - Re-wrote scoring method to use .inject instead of .each with an
      independant score variable, and broke out the triples calculation from
      the case statement to reduce repeated code

---
+++ lib/greed/score.rb
@@ -0,0 +1,45 @@
+module Greed 
+  def calc_score(dice)
+    # Catch nil parameter
+    raise ArgumentError, "Dice must be provided" unless dice
+    countHash = count_dice_results(dice)
+    calculate_score(countHash)
+  end
+
+  # Create a hash where key = die face and value = number of occurences
+  private
+    def count_dice_results(dice)
+      hash = Hash.new(0)
+      dice.each do |num|
+        hash[num] += 1
+      end
+      hash
+    end
+
+  # Calculate score based on GREED_RULES.txt
+  private
+    def calculate_score(hash)
+      hash.inject(0) do |score, (die, count)|
+
+        # Remaining is number of dice not part of a triple
+        remaining = count % 3
+
+        # 100 points for any individual 1's (not in a set of three)
+        # 50 points for any indivudal 5's (not in a set of three)
+        individualScore = case die
+                   when 1 then remaining * 100
+                   when 5 then remaining * 50
+                   else 0
+                   end
+
+        # Three 1's is worth 1000, not 100
+        die = 10 if die == 1
+        
+        # (100 * face value) points for any set of three
+        tripleScore = (count / 3) * (100 * die)
+
+        # Score is points-from-triples + points-from-individuals
+        score + tripleScore + individualScore
+      end
+    end
+end
